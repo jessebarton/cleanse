1d7df061cc1f3bed4b0fcab1332bf387429a3a1f
jessebarton <jessebarton95@gmail.com>
2018-10-23 01:04:17 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::string::ToString;
+use std::{i8, i16, i32, i64, u8, u16, u32, u64, isize, usize, f32, f64};
 
-struct Circle {
-    radius: i32
-}
-
-impl ToString for Circle {
-    fn to_string(&self) -> String {
-        format!("Circle of radius {:?}", self.radius)
-    }
-}
+use std::io::stdin;
 
 fn main() {
-    let circle = Circle { radius: 6 };
-    println!("{}", circle.to_string());
+    println!("Hello World!");
+
+    let num = 10;
+
+    let mut age: i32 = 23;
+    let let_x: char = 'x';
+    let is_it_true: bool = true;
+    println!("It is {0} that {1} is {0}", is_it_true, let_x);
+
+    println!("I am {} years old", age);
+
+    let (f_name, l_name) = ("Jesse", "Barton");
+    println!("My name is {} {}", f_name, l_name);
+    println!("Bin: {:b} Hex: {:x} Octa: {:o}", num, num, num);
 }
